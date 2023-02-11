5fed8b58d41a4c58314a90d12a5162f1a984090b
Erwin Seribo <erwin_rulezzz87@yahoo.com>
2023-02-11 18:12:51 +0800

    cover more test

--- spec/services/route_product_spec.rb
+++ spec/services/route_product_spec.rb
@@ -43,5 +43,32 @@ RSpec.describe RouteProduct do
         expect(product.reload.destination_id).to eq(destination.id)
       end
     end
+
+    context 'when no matching route' do
+      let!(:destination) { create(:destination, references: ['ref3', 'ref3'], categories: ['cat3', 'cat3']) }
+      let!(:destination_1) { create(:destination, references: ['ref4']) }
+      let!(:destination_2) { create(:destination, categories: ['cat4']) }
+      let!(:destination_3) { create(:destination, references: ['ref4'], categories: ['cat4']) }
+
+      it 'assign route successfully' do
+        subject
+
+        expect(product.reload.destination_id).to be_nil
+      end
+    end
+
+    context 'when matched by destination with max route' do
+      let!(:destination) { create(:destination, references: ['ref3', 'ref3'], categories: ['cat3', 'cat3']) }
+      let!(:destination_1) { create(:destination, references: ['ref4']) }
+      let!(:destination_2) { create(:destination, categories: ['cat4']) }
+      let!(:destination_3) { create(:destination, maximum_price: 101) }
+      let!(:destination_4) { create(:destination, references: ['ref4'], categories: ['cat4']) }
+
+      it 'assign route successfully' do
+        subject
+
+        expect(product.reload.destination_id).to eq(destination_3.id)
+      end
+    end
   end
 end
