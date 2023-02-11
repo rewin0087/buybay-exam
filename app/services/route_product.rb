d772f1f5131093803687ae3e776921864a4e7d9a
Erwin Seribo <erwin_rulezzz87@yahoo.com>
2023-02-11 18:01:06 +0800

    refactor

--- app/services/route_product.rb
+++ app/services/route_product.rb
@@ -1,6 +1,7 @@
 class RouteProduct
   def initialize(product)
     @product = product
+    @model = Destination
   end
 
   def call
@@ -9,29 +10,42 @@ class RouteProduct
 
   private
 
-    attr_reader :product
+    attr_reader :product, :query, :model
 
     def filtered_destination
-      query = Destination.where('destinations.references REGEXP ?', product.reference)
+      run_reference_query
+      run_category_query
+      run_max_price_query
+      sort_result
+    end
 
-      query = if (result = query.where('destinations.categories REGEXP ?', product.category)).count > 0
-        result
-      else
-        query.or(Destination.where('destinations.categories REGEXP ?', product.category))
-      end
+    def sort_result
+      query.sort_by {|d| [d.categories.count, d.references.count] }
+           .reverse
+           .first
+    end
 
-      query = if (result = query.where('destinations.maximum_price > ?', product.price)).count > 0
-         result
-       elsif (result = query.where('destinations.maximum_price IS NULL')).count > 0
-        result
-      elsif (result = query.or(Destination.where('destinations.maximum_price > ?', product.price))).count > 0
-        result
-      else
-        query
-      end
+    def run_reference_query
+      @query = model.where('destinations.references REGEXP ?', product.reference)
+    end
 
-      query = query.sort_by {|d| [d.categories.count, d.references.count] }
-                   .reverse
-                   .first
+    def run_category_query
+      @query = if (result = query.where('destinations.categories REGEXP ?', product.category)).count > 0
+                  result
+                else
+                  query.or(model.where('destinations.categories REGEXP ?', product.category))
+                end
+    end
+
+    def run_max_price_query
+      @query = if (result = query.where('destinations.maximum_price > ?', product.price)).count > 0
+                  result
+                elsif (result = query.where('destinations.maximum_price IS NULL')).count > 0
+                  result
+                elsif (result = query.or(model.where('destinations.maximum_price > ?', product.price))).count > 0
+                  result
+                else
+                  query
+                end
     end
 end
